59475c94fd98faed93de6232a3dd0420b1f36f2c
larsolesimonsen <larsolesimonsen@gmail.com>
2013-03-09 23:19:56 +0100

    - Minor edit. Testing out commit from inside webstorm

--- routes/index.js
+++ routes/index.js
@@ -18,11 +18,6 @@ exports.listGetJson = function(req, res) {
 		}
 		console.log('Got list from db: ' + (lists && lists.length > 0 && lists[0]));
 		var model = (lists && lists.length > 0 && lists[0]) || new ShoppingListModel({status: 'Open'});
-		// model.items = model.items || [];
-
-		// model.Items.push({ Text: 'Mælk', Multiplicity: '' + model.Items.length });
-		// model.Status = 'Open';
-		// model.save();
 
 		res.json(model);
 	});
@@ -41,7 +36,6 @@ exports.listGetJson = function(req, res) {
 			console.log('Error fetching shopping list from db ' + err);
 			res.json({});
 		}
-		console.log('Got list from db: ' + (lists && lists.length > 0 && lists[0]));
 		var model = (lists && lists.length > 0 && lists[0]) || new ShoppingListModel({_id: req.body._id, status: req.body.status});
 
 		var origItems = model.items;
@@ -49,9 +43,10 @@ exports.listGetJson = function(req, res) {
 
 		model.save(function(err, list) {
 			if(err) {
-				console.log('db save failed');
-				model.items = origItems;//Revert addition of new data if the db save failed
+				console.log('db save failed: ' + err.message);
+				model.items = origItems;//Revert addition of new list item if the db save failed
 			}
+            //Return the shopping list as it looks after the persisting has taken place
 			res.json(model);
 		});
  	});
